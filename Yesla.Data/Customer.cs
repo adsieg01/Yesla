f7ef3647d99ada3cdc5f7defbcdd9dfbe4ebf7c6
adsieg01 <adsieg01@gmail.com>
2017-12-05 18:08:38 -0500

    Database Entity

---
+++ Yesla.Data/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Yesla.Data
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+
+
+    }
+}
