e3d5d16ab9320a303e3a7e87d5cddcc308127b24
adsieg01 <adsieg01@gmail.com>
2017-12-05 16:41:35 -0500

    Intial Commit

--- Yesla.Web/Controllers/AccountController.cs
+++ Yesla.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Yesla.Web.Models;
+using Yesla.Data;
 
 namespace Yesla.Web.Controllers
 {
